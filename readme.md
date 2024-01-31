1b2210377acffbb7978fb3316f65705a338b7942
Mauricio Soares Vieira Miranda <87535941+SoaresMauricio-linera@users.noreply.github.com>
2024-01-31 15:55:59 -0300

    INITIAL COMMIT

---
+++ readme.md
@@ -0,0 +1,29 @@
+Instruções para executar a pre configuração do servidor linux.
+
+Primeiro instale o Git:
+
+```
+    sudo apt-get update
+
+    sudo apt-get install git-all
+
+    git version
+```
+
+depois clone o repositorio com o arquivo de configuração:
+
+```
+    git clone https://github.com/Linera-Team/VmConfig.git
+```
+
+e então execute
+
+```
+    chmod +x start.sh
+```
+
+em seguida:
+
+```
+    ./start.sh
+```
---
+++ start.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Update system package list
+sudo apt update
+
+# Install curl
+sudo apt-get install curl
+
+# Setup Node.js repository
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+
+# Install Node.js
+sudo apt-get install nodejs
+
+# Check Node.js version
+node -v
+
+# Install Yarn globally
+npm install yarn -g
+
+# Check Yarn version
+yarn -v
+
+# Setup GitHub CLI repository and install GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+# Authentication with GitHub CLI (requires user interaction)
+gh auth login
+
+# Remove potential conflicts with Docker packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+  sudo apt-get remove $pkg
+done
+
+# Add Docker’s official GPG key
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker repository to Apt sources
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker Engine, CLI, and plugins
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify Docker installation
+sudo docker run hello-world
+
+# Pull Node image from Docker Hub
+docker pull node
+
+# Clone specific GitHub repository
+gh repo clone Linera-Team/LineraGamesProvider
+
+# Navigate to the repository directory
+cd LineraGamesProvider/
+
+# Switch to a specific branch
+git switch 2.0
