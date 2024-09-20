0a8ccaf6d55d16c7aef0bb3416e6df52dcc22a82
Mauricio Soares Vieira Miranda <87535941+SoaresMauricio-linera@users.noreply.github.com>
2024-09-20 16:45:48 -0300

    Removido linhas desnecessarias

--- start.sh
+++ start.sh
@@ -32,25 +32,6 @@ sudo apt install gh
 # Authentication with GitHub CLI (requires user interaction)
 gh auth login
 
-# Remove potential conflicts with Docker packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
-  sudo apt-get remove $pkg
-done
-
-# Add Docker’s official GPG key
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add Docker repository to Apt sources
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
 # Clone specific GitHub repository
 gh repo clone Linera-Team/LineraGamesProvider
 
